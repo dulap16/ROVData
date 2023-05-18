c7462d4bf4bf495e1ee71da9fa49828353d08e88
dulap16 <tudorbrk8@yahoo.com>
2023-05-18 13:45:34 +0300

    Minor changes
    prefab color, symbol manager ease of use

--- Assets/SCRIPTS/SymbolManager.cs
+++ Assets/SCRIPTS/SymbolManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SymbolManager : MonoBehaviour
@@ -8,15 +9,18 @@ public class SymbolManager : MonoBehaviour
     public GameObject symbolGroup;
     public GameObject symbolPrefab;
 
+    private float width = 30;
+    private float height = 30;
     public float radius;
 
     // Start is called before the first frame update
     void Start()
     {
-        PoissonDiscSampler sampler = new PoissonDiscSampler(100, 100, 1);
+        PoissonDiscSampler sampler = new PoissonDiscSampler(30, 30, 0.3f);
         foreach (Vector2 sample in sampler.Samples())
         {
-            Instantiate(symbolPrefab, sample, Quaternion.identity, symbolGroup.transform);
+            Vector2 position = new Vector2(sample.x - width / 2, sample.y - height / 2);
+            Instantiate(symbolPrefab, position, Quaternion.identity, symbolGroup.transform);
         }
     }
 
