214bec773fe3be9aa0f03074cc410ead4cf351cc
dulap16 <tudorbrk8@yahoo.com>
2023-05-23 18:05:54 +0300

    Create class for judet

--- Assets/SCRIPTS/JudetChanger.cs
+++ Assets/SCRIPTS/JudetChanger.cs
@@ -1,13 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class JudetChanger : MonoBehaviour
 {
+    [Serializable]
+    public class texture
+    {
+        public Sprite full;
+        public Sprite transp;
+        public Vector3 poz;
+        public Vector3 scale;
+    }
+
+    [Serializable]
+    public class judet
+    {
+        public string nume;
+        public GameObject fbx;
+        public Vector3 poz;
+        public Vector3 scale;
+        public Vector3 rotation;
+        public texture texture;
+    }
+
+    public List<judet> judete;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(Application.streamingAssetsPath);
+        Debug.Log(Application.persistentDataPath);
     }
 
     // Update is called once per frame
