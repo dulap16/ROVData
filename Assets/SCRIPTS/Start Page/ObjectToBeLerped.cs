71ba76fa252702f7c381e739cf2d265b4032db48
dulap16 <tudorbrk8@yahoo.com>
2023-09-12 20:10:45 +0300

    Add scripts from the Unity Lerper repository

---
+++ Assets/SCRIPTS/Start Page/ObjectToBeLerped.cs
@@ -0,0 +1,123 @@
+﻿using Assets.SCRIPTS.Start_Page.Lerpers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.SCRIPTS.Start_Page
+{
+    public class ObjectToBeLerped : MonoBehaviour
+    {
+        private GameObject go;
+
+        [SerializeField] private StageManager stages;
+
+        private Dictionary<Lerper, IEnumerator> coroutines;
+        private Stage currentStage;
+
+        public ObjectToBeLerped(GameObject _go)
+        {
+            go = _go;
+
+            Debug.Log(go);
+        }
+
+        IEnumerator ApplyDelayToCurrentStage(Lerper l, float delay)
+        {
+            yield return new WaitForSeconds(delay);
+
+            l.StartLerping();
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            if (go == null)
+                go = gameObject;
+
+
+            currentStage = stages.getCurrentStage();
+            currentStage.setInitIfCase(go.transform.localPosition, go.transform.localScale, go.GetComponent<SpriteRenderer>().color, go.transform.rotation);
+
+            coroutines = new Dictionary<Lerper, IEnumerator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            UpdateLerpingProperties();
+            ModifyAccordingToLerp();
+
+            if (stages.advanceIfCase())
+            {
+                ResetCurrentVariables();
+
+                MakeNextStageStartFromLast();
+
+                ResetCurrentVariables();
+                StartLerping();
+            }
+        }
+
+        public void StartLerping()
+        {
+            foreach (Lerper l in currentStage.lerperDict.Values)
+            {
+                l.GoToBeginning();
+                coroutines[l] = ApplyDelayToCurrentStage(l, l.delay);
+                StartCoroutine(coroutines[l]);
+            }
+        }
+
+        public void ModifyAccordingToLerp()
+        {
+            if (currentStage.willLerpProperty("position"))
+                go.transform.localPosition = ((Vector3Lerper)currentStage.getLerper("position")).getCurrentValue();
+
+            if (currentStage.willLerpProperty("scale"))
+                go.transform.localScale = ((Vector3Lerper)currentStage.getLerper("scale")).getCurrentValue();
+
+            if (currentStage.willLerpProperty("color"))
+                go.GetComponent<SpriteRenderer>().color = ((ColorLerper)currentStage.getLerper("color")).getCurrentValue();
+
+            if (currentStage.willLerpProperty("rotation"))
+                go.transform.rotation = ((QuaternionLerper)currentStage.getLerper("rotation")).getCurrentValue();
+        }
+
+        public void StopAll()
+        {
+            currentStage.StopAll();
+        }
+
+        public void ResetCurrentVariables()
+        {
+            currentStage = stages.getCurrentStage();
+        }
+
+        public void Restart()
+        {
+            foreach (Lerper l in currentStage.lerperDict.Values)
+            {
+                StopCoroutine(coroutines[l]);
+            }
+
+            stages.Restart();
+            ResetCurrentVariables();
+            StartLerping();
+        }
+
+        public void UpdateLerpingProperties()
+        {
+            currentStage.LerpAll();
+        }
+
+        public void MakeStageInheritFromLast(Stage last, Stage next)
+        {
+            next.setInitIfCase(last.getLerper("position").finalVector3(), last.getLerper("scale").finalVector3(), last.getLerper("color").finalColor(), last.getLerper("rotation").finalQuaternion());
+        }
+
+        public void MakeNextStageStartFromLast()
+        {
+            MakeStageInheritFromLast(stages.getStageOfIndex(stages.getCurrentIndex() - 1), stages.getStageOfIndex(stages.getCurrentIndex()));
+        }
+    }
+}
---
+++ Assets/SCRIPTS/Start Page/Stage.cs
@@ -0,0 +1,144 @@
+﻿using Assets.SCRIPTS.Start_Page.Lerpers;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.SCRIPTS.Start_Page
+{
+    [Serializable]
+    public class Stage
+    {
+        [SerializeField] private Vector3Lerper positionLerper;
+        [SerializeField] private Vector3Lerper scaleLerper;
+        [SerializeField] private ColorLerper colorLerper;
+        [SerializeField] private QuaternionLerper rotationLerper;
+
+        private List<Lerper> lerpers;
+        public Dictionary<String, Lerper> lerperDict;
+
+        public Stage()
+        {
+            lerpers = new List<Lerper>();
+
+            positionLerper = new Vector3Lerper();
+            scaleLerper = new Vector3Lerper();
+            colorLerper = new ColorLerper();
+            rotationLerper = new QuaternionLerper();
+            lerpers.Add(positionLerper);
+            lerpers.Add(scaleLerper);
+            lerpers.Add(colorLerper);
+            lerpers.Add(rotationLerper);
+
+            lerperDict = new Dictionary<String, Lerper>();
+            lerperDict.Add("position", positionLerper);
+            lerperDict.Add("scale", scaleLerper);
+            lerperDict.Add("color", colorLerper);
+            lerperDict.Add("rotation", rotationLerper);
+        }
+
+        public Lerper getLerper(String name)
+        {
+            return lerperDict[name];
+        }
+
+        public void LerpAll()
+        {
+            if(positionLerper.WillLerp())
+                positionLerper.Lerp();
+
+            if(scaleLerper.WillLerp())
+                scaleLerper.Lerp();
+            
+            if(colorLerper.WillLerp())
+                colorLerper.Lerp();
+            
+            if(rotationLerper.WillLerp())
+                rotationLerper.Lerp();
+
+            /*foreach (Lerper l in lerpers)
+                if (l.WillLerp())
+                {
+                    Debug.Log(l);
+                    l.Lerp();
+                }*/
+        }
+
+        public void StartAll()
+        {
+            positionLerper.StartLerping();
+            scaleLerper.StartLerping();
+            colorLerper.StartLerping();
+            rotationLerper.StartLerping();
+
+            /*foreach (Lerper l in lerpers)
+                l.StartLerping();*/
+        }
+
+        public void StopAll()
+        {
+            positionLerper.StopLerping();
+            scaleLerper.StopLerping();
+            colorLerper.StopLerping();
+            rotationLerper.StopLerping();
+
+            /*foreach (Lerper l in lerpers)
+                l.StopLerping();*/
+        }
+
+        public void RestartAll()
+        {
+            positionLerper.Restart();
+            scaleLerper.Restart();
+            colorLerper.Restart();
+            rotationLerper.Restart();
+
+            /*foreach (Lerper l in lerpers)
+                l.Restart(); */
+        }
+
+        public void GoToBeginning()
+        {
+            positionLerper.GoToBeginning();
+            scaleLerper.GoToBeginning();
+            colorLerper.GoToBeginning();
+            rotationLerper.GoToBeginning();
+
+            /*foreach (Lerper l in lerpers)
+                l.GoToBeginning();*/
+        }
+
+        public bool wasStageFinished()
+        {
+            return positionLerper.wasTargetReached()
+                && scaleLerper.wasTargetReached()
+                && colorLerper.wasTargetReached()
+                && rotationLerper.wasTargetReached();
+        }
+
+        public bool willLerpProperty(String name)
+        {
+            return lerperDict[name].WillLerp();
+        }
+
+        public void setInitValuesOfStage(Vector3 pos, Vector3 scale, Color color, Quaternion rotation)
+        {
+            positionLerper.setInit(pos);
+            scaleLerper.setInit(scale);
+            colorLerper.setInit(color);
+            rotationLerper.setInit(rotation);
+        }
+
+        public void setInitIfCase(Vector3 pos, Vector3 scale, Color color, Quaternion rotation)
+        {
+            if(positionLerper.willInheritLast())
+                positionLerper.setInit(pos);
+            if (scaleLerper.willInheritLast())
+                scaleLerper.setInit(scale);
+            if (colorLerper.willInheritLast())
+                colorLerper.setInit(color);
+            if (rotationLerper.willInheritLast())
+                rotationLerper.setInit(rotation);
+        }
+    }
+}
