bd136d9a843477d45406f804f4bb778e51b7ac70
dulap16 <tudorbrk8@yahoo.com>
2023-09-12 20:11:21 +0300

    Add manager for the start animation

--- Assets/SCRIPTS/Start Page/StartManager.cs
+++ Assets/SCRIPTS/Start Page/StartManager.cs
@@ -1,18 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Assets.SCRIPTS.Start_Page;
 
 public class StartManager : MonoBehaviour
 {
+    [SerializeField] public Dictionary<GameObject, ObjectToBeLerped> lerpedObjects;
+
+    /// GAMEOBJECTS, SPRITE RENDERERS AND COLORS
+    public GameObject lowerCover;
+    public GameObject upperCover;
+    public GameObject graphCover;
+    public GameObject logo;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        lerpedObjects = new Dictionary<GameObject, ObjectToBeLerped>();
+        /*lerpedObjects.Add(lowerCover, lowerCover.GetComponent<ObjectToBeLerped>());
+        lerpedObjects.Add(upperCover, upperCover.GetComponent<ObjectToBeLerped>());
+        lerpedObjects.Add(graphCover, graphCover.GetComponent<ObjectToBeLerped>());*/
+        lerpedObjects.Add(logo,  logo.GetComponent<ObjectToBeLerped>());
+
+        StartLerpingAllObjects();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown("space"))
+            Restart();
+    }
+
+    public void StartLerpingObject(GameObject go)
+    {
+        lerpedObjects[go].StartLerping();
+    }
+
+    public void StartLerpingAllObjects()
+    {
+        foreach (ObjectToBeLerped obj in lerpedObjects.Values)
+        {
+            obj.StartLerping();
+        }
+    }
+
+    public void Restart()
+    {
+        foreach(ObjectToBeLerped obj in lerpedObjects.Values)
+        {
+            obj.Restart();
+        }
     }
 }
