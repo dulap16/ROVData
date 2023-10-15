95261ba0ad50b4d806d9a1917121bca3f1d23987
dulap16 <tudorbrk8@yahoo.com>
2023-10-15 11:18:31 +0300

    Values from the previous "SliderManager.cs"

--- Assets/SCRIPTS/Clean Scripts/LimitsManager.cs
+++ Assets/SCRIPTS/Clean Scripts/LimitsManager.cs
@@ -1,10 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LimitsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Slider lower;
+    public Slider upper;
+
+    public TMP_InputField lowerText;
+    public TMP_InputField upperText;
+
+    public CheckColliderBehaviour lowerColl;
+    public CheckColliderBehaviour upperColl;
+
+    public Handler h;
+
+
+    public int _lowerValue;
+    public int _upperValue;
+
+
     void Start()
     {
         
