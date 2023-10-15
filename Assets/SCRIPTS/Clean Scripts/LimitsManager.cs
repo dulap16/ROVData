c75843748a44fa2b3fdfaa97c21eeaf5c0daa969
dulap16 <tudorbrk8@yahoo.com>
2023-10-15 11:18:55 +0300

    Cleaner version, with cleaner methods and variable names

--- Assets/SCRIPTS/Clean Scripts/LimitsManager.cs
+++ Assets/SCRIPTS/Clean Scripts/LimitsManager.cs
@@ -24,12 +24,82 @@ public class LimitsManager : MonoBehaviour
 
     void Start()
     {
-        
+        lowerColl = lower.gameObject.GetComponent<CheckColliderBehaviour>();
+        upperColl = upper.gameObject.GetComponent<CheckColliderBehaviour>();
+
+        Reset();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (lowerColl.clicked == true || upperColl.clicked == true)
+        {
+            /*lowerSliderMoved();
+            upperSliderMoved();
+
+            equalizeValues();
+            h.ValuesClick();*/
+            // WON'T CALL METHODS HERE AS IT IS TOO INEFFICIENT
+
+            _lowerValue = (int)(h.max * lower.normalizedValue);
+            _upperValue = (int)(h.max * upper.normalizedValue);
+
+            lowerText.text = _lowerValue.ToString();
+            upperText.text = _upperValue.ToString();
+
+            h.ValuesClick();
+        }
+    }
+
+    public void setLowerValue(int val)
+    {
+        _lowerValue = val;
+
+        equalizeValues();
+        h.ValuesClick();
+    }
+
+    public void setUpperValue(int val)
+    {
+        _upperValue = val;
+
+        equalizeValues();
+        h.ValuesClick();
+    }
+
+
+    public void lowerSliderMoved()
+    {
+        setLowerValue((int)(h.max * lower.normalizedValue));
+    }
+
+    public void upperSliderMoved()
+    {
+        setUpperValue((int)(h.max * upper.normalizedValue));
+    }
+
+    public void lowerTextChanged()
+    {
+        setLowerValue(int.Parse(lowerText.text));
+    }
+
+    public void upperTextChanged()
+    {
+        setUpperValue(int.Parse(upperText.text));
+    }
+
+    public void Reset()
+    {
+        setLowerValue(0);
+        setUpperValue(h.max);
+    }
+
+    private void equalizeValues()
+    {
+        lowerText.text = _lowerValue.ToString();
+        upperText.text = _upperValue.ToString();
+
+        lower.normalizedValue = (float)_lowerValue / (float)h.max;
+        upper.normalizedValue = (float)_upperValue / (float)h.max;
     }
 }
