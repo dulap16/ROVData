862718730c928d17e51fad8fa8b8ff274f2793af
dulap16 <tudorbrk8@yahoo.com>
2023-10-15 09:14:43 +0300

    Add Segment class
    class that has a UI component, its initial and hidden position
    Will later be made to lerp to every position

--- Assets/SCRIPTS/new ui/NewUIHandler.cs
+++ Assets/SCRIPTS/new ui/NewUIHandler.cs
@@ -18,6 +18,38 @@ public class NewUIHandler : MonoBehaviour
 
     public Handler mainHandler;
     public TMP_Text selectedRegionTMP;
+
+    public Vector3 hiddenPos;
+
+    [Serializable]
+    public class Segment
+    {
+        public String name;
+        public GameObject _go;
+        public Vector3 initPos;
+        private Vector3 hiddenPos = new Vector3(1000, 1000, 0);
+
+        public void show(bool b)
+        {
+            if (b)
+                _go.transform.localPosition = initPos;
+            if (b)
+                _go.transform.localPosition = hiddenPos;
+        }
+
+        public void _init()
+        {
+            initPos = _go.transform.localPosition;
+            
+        }
+
+        public void _init(Vector3 pos)
+        {
+            initPos = pos;
+        }
+    }
+
+    [SerializeField] public List<Segment> segments;
     
     // Start is called before the first frame update
     void Start()
