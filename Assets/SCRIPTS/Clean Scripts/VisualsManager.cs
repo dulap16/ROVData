85a5ad5aec1775af2cfc9aa94e2b9a4899262414
dulap16 <tudorbrk8@yahoo.com>
2023-10-22 22:54:42 +0300

    Update VisualsManager.cs

--- Assets/SCRIPTS/Clean Scripts/VisualsManager.cs
+++ Assets/SCRIPTS/Clean Scripts/VisualsManager.cs
@@ -1,18 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VisualsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Handler handler;
+
+    // CADASTRE SETTINGS
+    public GameObject cadastre = null;
+    private Material cadMat;
+    private Vector3 initCadPos;
+    private Vector3 targetCadPos;
+
+    public Slider cadOpacSlider;
+    public TMP_Text cadOpacTag;
+
+
+    // PRESENTATION MODE
+    public RectTransform map;
+    public SpriteRenderer texture;
+    public Sprite fullImage;
+    public Sprite transparentImage;
+    public bool presentation = false;
+
+    private Vector3 initMapPos;
+    public Vector3 presMapPos;
+
+    // TITLE
+    public TMP_InputField titleIF;
+    public TMP_Text title;
+
     void Start()
     {
-        
+        handler = GameObject.Find("Handler").GetComponent<Handler>();
+        cadMat = cadastre.GetComponent<Renderer>().material;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void JudetChanged()
     {
-        
+        cadMat = cadastre.GetComponent<Renderer>().material;
+
+        // CADASTRE
+        initCadPos = cadastre.transform.localPosition;
+        targetCadPos = new Vector3(500, 0, 0);
+
+        titleIF.text = handler.curentJudet;
+        title.text = handler.curentJudet;
+    }
+
+    // CADASTRE
+    public void SliderSlid()
+    {
+        float val = Mathf.Round(cadOpacSlider.normalizedValue * 100f) / 100f;
+        cadOpacTag.text = val.ToString();
+        setCadOpac(val);
+    }
+
+
+    public void setCadOpac(float val)
+    {
+        Color c = cadMat.color;
+        c.a = val;
+        cadMat.color = c;
+    }
+
+
+
+    // TITLE
+    public void titleIFChanged()
+    {
+        changeTitle(titleIF.text);
+    }
+
+    public void changeTitle(string t) 
+    {
+        title.text = t;
     }
 }
