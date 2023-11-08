c1414ed7d216603a8944f689b071aec4f568b59f
dulap16 <tudorbrk8@yahoo.com>
2023-11-08 21:30:24 +0200

    enlargeMapScale(float howMuch)

--- Assets/SCRIPTS/ZoomManager.cs
+++ Assets/SCRIPTS/ZoomManager.cs
@@ -61,6 +61,11 @@ public class ZoomManager : MonoBehaviour
     {
         map.transform.position = mapInitCenter;
         map.transform.localScale = mapInitScale;
+    private void enlargeMapScale(float howMuch)
+    {
+        changeMapScale(map.transform.localScale * howMuch);
+    }
+
     private void changeMapScale(Vector3 newScale)
     {
         map.transform.localScale = newScale;
