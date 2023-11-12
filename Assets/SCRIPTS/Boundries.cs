90edca5cb5b0db6373d9fec85c6ef8b0b4bf7d4a
dulap16 <tudorbrk8@yahoo.com>
2023-11-12 13:10:29 +0200

    Boundries class for getting the corners of a go

---
+++ Assets/SCRIPTS/Boundries.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.SCRIPTS
+{
+    [Serializable]
+    public class Boundries
+    {
+        [SerializeField] public float north, east, south, west;
+
+        public bool isThisWithinBoundries(Boundries container)
+        {
+            if (north < container.north && south > container.south && east < container.east && west > container.west)
+                return true;
+
+            return false;
+        }
+    }
+}
